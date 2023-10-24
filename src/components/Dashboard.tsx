f885262846ae2ef2adfa72cfad1369f3cd9c1b31
koldovsky <koldovsky@gmail.com>
2023-10-24 20:00:18 +0300

    Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import { trpc } from "@/app/api/_trpc/client";
 import UploadButton from "./UploadButton";
+import ShouldRender from "./ShouldRender";
+import { Ghost } from "lucide-react";
 
 const Dashboard = () => {
+    const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
     return (
         <main className='mx-auto max-w-7xl md:p-10'>
             <div className='mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0'>
@@ -9,6 +16,19 @@ const Dashboard = () => {
                 </h1>
                 <UploadButton />
             </div>
+            <ShouldRender if={files && files.length > 0}>
+                <div></div>
+            </ShouldRender>
+            <ShouldRender if={isLoading}>
+                <div></div>
+            </ShouldRender>
+            <ShouldRender if={!files || files?.length === 0}>
+                <div className="mt-16 flex flex-col items-center gap-2">
+                    <Ghost className="h-8 w-8 text-zinc-800" />
+                    <h3 className="font-semibold text-xl">Pretty empty around here</h3>
+                    <p>Let&apos;s upload your PDF</p>
+                </div>
+            </ShouldRender>
         </main>
     );
 }
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -9,17 +9,24 @@ const UploadButton = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
-        <Dialog open={isOpen}
-            onOpenChange={(open) => { if (!open) setIsOpen(open) }}
-        >
-            <DialogTrigger asChild onClick={() => setIsOpen(true)}>
-                <Button>Upload PDF</Button>
-            </DialogTrigger>
-            <DialogContent>
-                dialog content
-            </DialogContent>
+        <Dialog
+          open={isOpen}
+          onOpenChange={(v) => {
+            if (!v) {
+              setIsOpen(v)
+            }
+          }}>
+          <DialogTrigger
+            onClick={() => setIsOpen(true)}
+            asChild>
+            <Button>Upload PDF</Button>
+          </DialogTrigger>
+    
+          <DialogContent>
+            dialog content
+          </DialogContent>
         </Dialog>
-    )
+      )
 }
 
 export default UploadButton;
